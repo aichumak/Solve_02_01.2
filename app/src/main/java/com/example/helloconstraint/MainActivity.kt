db352d0180fa4e7526060aa4728189aaeed125f6
aichumak <aichumak.dev@gmail.com>
2022-04-04 22:37:56 +0300

    Fin commit

--- app/src/main/java/com/example/helloconstraint/MainActivity.kt
+++ app/src/main/java/com/example/helloconstraint/MainActivity.kt
@@ -7,7 +7,7 @@ import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import com.example.hellotoast.R
+import helloconstraint.R
 
 class MainActivity : AppCompatActivity() {
     private var mCount = 0
