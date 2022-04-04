ef4a0b238b63df9e27c9f1241aab5eb164193db2
aichumak.dev@gmail.com <aichumak.dev@gmail.com>
2022-04-04 17:17:36 +0300

    init

--- app/src/main/java/com/example/hellotoast/MainActivity.kt
+++ app/src/main/java/com/example/hellotoast/MainActivity.kt
@@ -1,19 +1,27 @@
 package com.example.hellotoast
 
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Color
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     private var mCount = 0
     private lateinit var mShowCount: TextView
+    private lateinit var buttonZero: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        initViews()
+    }
+
+    private fun initViews() {
         mShowCount = findViewById(R.id.show_count)
+        buttonZero = findViewById(R.id.button_zero)
     }
 
     fun showToast(view: View) {
@@ -22,6 +30,23 @@ class MainActivity : AppCompatActivity() {
 
     fun countUp(view: View) {
         mCount++
+        if (mCount % 2 == 0) {
+            view.setBackgroundColor(Color.GREEN)
+        } else {
+            view.setBackgroundColor(Color.BLUE)
+        }
+        buttonZero.setBackgroundColor(Color.MAGENTA)
+        showCount()
+    }
+
+    fun countToZero(view: View) {
+        mCount = 0
+        view.setBackgroundColor(Color.GRAY)
+        showCount()
+    }
+
+    private fun showCount() {
         mShowCount.text = mCount.toString()
     }
+
 }
