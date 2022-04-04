bd46f3fe6a1ccd97b1c6cdebe7b3ec384f789830
aichumak.dev@gmail.com <aichumak.dev@gmail.com>
2022-04-04 22:27:31 +0300

    fin

--- app/src/main/java/com/example/helloconstraint/MainActivity.kt
+++ app/src/main/java/com/example/helloconstraint/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.hellotoast
+package com.example.helloconstraint
 
 import android.graphics.Color
 import android.os.Bundle
@@ -7,6 +7,7 @@ import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.example.hellotoast.R
 
 class MainActivity : AppCompatActivity() {
     private var mCount = 0
